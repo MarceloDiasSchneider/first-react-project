929b9898709a97eccab26a03f8006c6518c5bc80
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-01-15 18:13:32 +0100

    setup bootstrap 5

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Container from './components/Container';
 import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
